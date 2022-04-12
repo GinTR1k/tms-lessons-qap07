5f7aaf17c224c4e88e705caaf7ce9f20bd239a68
Alexander Karateev <administrator@gintr1k.space>
2022-04-12 21:18:55 +0300

    добавлен код с первого урока

---
+++ lesson1/lesson1.py
@@ -0,0 +1,35 @@
+# print
+print(2 + 2)
+print(2 * 11)
+print((2 + 2) * 2)
+print(2 - 2)
+print(1 / 2)
+print(3 ** 2)
+
+print(3.0 ** 2)
+print(2.0 + 2 + 1 + 0 - 20 * 73)
+print(3.0 == 3)
+
+print()
+
+# str, int, float, bool
+print(int(1))
+print(float(1))
+print(int(1.99999))
+
+print('abc123' == '1')
+print("qwerty" * 2)
+print('123')
+print(int('897234'))
+print(type(int('897234')))
+print(str(-8437645))
+print(str(False))  # print(False)
+
+print(False == 0)
+print(True == 1)
+print(int(True))
+
+# 1. Написать гуглопочту
+# 2. скачать python 3.8 или выше (3.10 актуальная версия)
+# 3. скачать ide
+
